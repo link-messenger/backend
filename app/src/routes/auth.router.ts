135fd243c5bf49b97c88c23cad2a0c5bbdfa640f
mjn81 <mjjnn81@gmail.com>
2023-03-10 19:03:02 +0330

    [ADD] logout route added.

--- app/src/routes/auth.router.ts
+++ app/src/routes/auth.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { loginController, profileController, registerController } from '../controllers';
+import { loginController, logoutController, profileController, registerController } from '../controllers';
 import { protectedRoute, validate } from '../middlewares';
 import { loginSchema, registerSchema } from '../schemas';
 
@@ -8,5 +8,5 @@ const router = Router();
 router.post('/login', validate(loginSchema), loginController);
 router.post('/register', validate(registerSchema), registerController);
 router.get('/me', protectedRoute, profileController);
-// router.get('/logout', protectedRoute, )
+router.get('/logout', protectedRoute, logoutController);
 export const authRouter = router;
