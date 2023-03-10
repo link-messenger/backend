04e8d875a76badc93dd8f9ef37943fe942e71255
mjn81 <mjjnn81@gmail.com>
2023-03-10 18:59:46 +0330

    [ADD] redis user counter added.

--- app/src/config/socket.ts
+++ app/src/config/socket.ts
@@ -1,8 +1,15 @@
 import { Socket } from 'socket.io';
+import { UnauthorizedError } from '../errors';
 import { deleteMessage, editMessage, sendMesssage } from '../realtime';
+import { generateRedisTokenName } from '../utils';
+import { getRedisClient } from './db';
 
 export const onConnnect = async (socket: Socket) => {
 	const uid: string = socket.handshake.auth.id;
+	const redis = getRedisClient();
+	const tokenIsValid = await redis.get(generateRedisTokenName(uid));
+	await redis.incr('user-counter');
+	if (!tokenIsValid) throw new UnauthorizedError('Invalid Token');
 	socket.join(uid);
 	socket
 		.on('send-message', (message) =>
@@ -25,5 +32,8 @@ export const onConnnect = async (socket: Socket) => {
 				message,
 				uid,
 			})
-		);
+		)
+		.on('disconnect', async () => {
+			await redis.decr('user-counter');
+		});
 };
